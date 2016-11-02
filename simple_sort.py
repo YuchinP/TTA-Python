f0922ad4d9dd4efd95616f0f0b700b23f49558b8
YuchinP <yuchinspan@gmail.com>
2016-11-02 13:19:50 -0700

    Add files via upload
    First attempt at hand-made sort

---
+++ simple_sort.py
@@ -0,0 +1,15 @@
+#listA = [67,45,2,13,1,998]
+listA = [89,23,33,45,10,12,45,45,45]
+y = (9999)
+listB = [y]
+
+
+for y in listA:
+    while len(listA) != 0:
+        listB.append(min(listA))
+        listA.remove(min(listA))  
+else:
+    listB.remove(9999)
+    print(listB)
+
+
