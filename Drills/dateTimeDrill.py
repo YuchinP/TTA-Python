9bd125028c414ba53caea6058805a4b13e7f7144
YuchinP <yuchinspan@gmail.com>
2016-11-09 14:45:44 -0800

    Update dateTimeDrill.py
    Small fixes and efficiency improvements

--- Drills/dateTimeDrill.py
+++ Drills/dateTimeDrill.py
@@ -13,21 +13,18 @@
 ##Print out if each of the two branches are open or closed.
 
 ##Modules
-import datetime
 from datetime import *
-import pytz
 from pytz import *
 
 ##Compares the current hour of the branch to whether or not they fit in the open range
 def is_it_closed(x):
+    ##Defines the hours the branches are open and closed
+    branch_close = time(hour=21,minute=0,second=0)
+    branch_open = time(hour=9,minute=0,second=0)  
     if branch_open <= x and x <= branch_close:
         print ('This Location is Open!')
     else:
-        print('This Location is Closed!')
-
-##Defines the hours the branches are open and closed
-branch_close = time(hour=22,minute=0,second=0)
-branch_open = time(hour=9,minute=0,second=0)        
+        print('This Location is Closed!')  
 
 ##Defines the 3 timezones that are going to be used
 utc_time = datetime.utcnow()
@@ -36,9 +33,9 @@ etz = timezone('America/New_York')
 ltz = timezone('Europe/London')
 
 ##Converts the general UTC time to the correct timezone needed
-P = pytz.utc.localize(utc_time).astimezone(tz)
-NY = pytz.utc.localize(utc_time).astimezone(etz)
-LON = pytz.utc.localize(utc_time).astimezone(ltz)
+P = utc.localize(utc_time).astimezone(tz)
+NY = utc.localize(utc_time).astimezone(etz)
+LON = utc.localize(utc_time).astimezone(ltz)
 
 ##Converts the datetime objects to time types
 Portland = time(hour = P.hour) 
