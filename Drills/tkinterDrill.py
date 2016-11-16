887eb3fabedb311b65e12b69f603e4a380e86105
Yuchin Pan <yuchinspan@gmail.com>
2016-11-16 12:08:24 -0800

    Tkinter UI Final
    Working program and UI

--- Drills/tkinterDrill.py
+++ Drills/tkinterDrill.py
@@ -11,33 +11,46 @@ class ShutilUI(Frame):
         Frame.__init__(self,root)
 
         #   Title of the UI
-        label = ttk.Label(root, text = "File Moving UI", justify = CENTER, font = ('Courier', 13, 'bold'))
-        label.pack()
+        self.label = ttk.Label(root, text = "File Moving UI", justify = CENTER, font = ('Courier', 13, 'bold'))
+        self.label.pack()
 
         #   Creation of the paned window
-        panedwindow = ttk.Panedwindow(root, orient = VERTICAL)
-        panedwindow.pack(fill = BOTH, expand = True)
-        frame1 = ttk.Frame(panedwindow, width = 600, height = 400, relief = SUNKEN)
-        frame2 = ttk.Frame(panedwindow, width = 600, height = 200, relief = SUNKEN)
-        panedwindow.add(frame1, weight = 1)
-        panedwindow.add(frame2, weight = 4)
+        self.panedwindow = ttk.Panedwindow(root, orient = VERTICAL)
+        self.panedwindow.pack(expand = False)
+        self.frame1 = ttk.Frame(self.panedwindow, width = 600, height = 400)
+        self.frame2 = ttk.Frame(self.panedwindow, width = 600, height = 200)
+        self.frame3 = ttk.Frame(self.panedwindow, width = 600, height = 50, relief = SUNKEN)
+        self.frame4 = ttk.Frame(self.panedwindow, width = 600, height = 50, relief = SUNKEN)
+        self.panedwindow.add(self.frame1, weight = 1)
+        self.panedwindow.add(self.frame2, weight = 4)
+        self.panedwindow.add(self.frame3, weight = 4)
+        self.panedwindow.add(self.frame4, weight = 4)
+
+        #   Frame1 instructions
+        self.instructions = ttk.Label(self.frame1, text = "In order to execute the file transfer for files modified within the last 24 hours, please select an initial and destination folder. You may check eligible files for the file transfer with the 'Show Eligible Files' button after selecting an initial folder", justify = CENTER, font = ('Courier', 12))
+        self.instructions.config(wraplength = 600)
+        self.instructions.pack()
+
+        #   Source/Destinations
+        self.field1 = ttk.Label(self.frame3, text = "Source Folder:")
+        self.field2 = ttk.Label(self.frame4, text = "Destination Folder:")
+        self.field1.grid()
+        self.field2.grid()
         
         #   Define buttons
-        browse = ttk.Button(frame2, text = "Browse Initial Folder", command = lambda:tkinterDrillFunc.folderselect1(self))
-        browse2 = ttk.Button(frame2, text = "Browse Destination Folder", command = lambda:tkinterDrillFunc.folderselect2(self))
-        button = ttk.Button(frame2, text = "Show Eligible Files", command = lambda:tkinterDrillFunc.show_files(self))
-        executer = ttk.Button(frame2, text = "Execute File Transfer", command = lambda:tkinterDrillFunc.the_mover(self))
-
+        self.browse = ttk.Button(self.frame2, text = "Browse Initial Folder", command = lambda:tkinterDrillFunc.folderselect1(self))
+        self.browse2 = ttk.Button(self.frame2, text = "Browse Destination Folder", command = lambda:tkinterDrillFunc.folderselect2(self))
+        self.button = ttk.Button(self.frame2, text = "Show Eligible Files", command = lambda:tkinterDrillFunc.show_files(self), state = 'disabled')
+        self.executer = ttk.Button(self.frame2, text = "Execute File Transfer", command = lambda:tkinterDrillFunc.the_mover(self), state = 'disabled')
 
         #   Placing buttons down on grid
-        browse.grid(row = 0, column = 1, padx = 40)
-        browse2.grid(row = 0, column = 2, padx = 40)
-        button.grid(row = 0, column = 3, padx = 40)
-        executer.grid(row = 0, column = 4, padx = 40)
-
+        self.browse.grid(row = 0, column = 1, padx = 40)
+        self.browse2.grid(row = 0, column = 2, padx = 40)
+        self.button.grid(row = 0, column = 3, padx = 40)
+        self.executer.grid(row = 0, column = 4, padx = 40)
         
-        path = ''
-
+        self.path = ''      
+        self.destination = ''
         
 if __name__=='__main__':
     root = Tk()
--- Drills/tkinterDrillFunc.py
+++ Drills/tkinterDrillFunc.py
@@ -12,12 +12,17 @@ def folderselect1(self):
     Sel_directory1 = filedialog.askdirectory()
     print(Sel_directory1)
     self.path = Sel_directory1
+    file_activator(self)
+    execute_activator(self)
+    show_source(self)
     
         
 def folderselect2(self):
     Sel_directory2 = filedialog.askdirectory()
     print(Sel_directory2)
-    destination = Sel_directory2
+    self.destination = Sel_directory2
+    execute_activator(self)
+    show_destination(self)
              
 def get_time(self,files):
     os.chdir(self.path)
@@ -29,30 +34,55 @@ def get_time(self,files):
     return ntimer
 
 def show_files(self):
-    source = self.path
+    con_prompt(self)
     for files in os.listdir(self.path):
         if files.endswith('.txt'):
             get_time(self,files)
-            if get_time(self,files) > 86400:
+            if get_time(self,files) < 86400:
                 print (files)
+                output = files
+                self.files = ttk.Label(self.window, text = output, justify = CENTER, font = ('Courier', 13, 'bold')).pack()
             else:
-               print('ineligible')
-        
+                print('Ineligible')
 
-def the_mover(src, dest):
-    source = path
-    for files in path:
+        
+def the_mover(self):
+    source = self.path
+    con_prompt(self)
+    for files in os.listdir(self.path):
         if files.endswith('.txt'):
-            get_time(files)
-            if get_time(files) > 86400:
-                shutil.move(files, destination)
-                print('Text files from', source, 'have been moved to', destination)
+            get_time(self,files)
+            if get_time(self,files) < 86400:
+                shutil.move(files, self.destination)
+                confirmation = 'Text files from', source, 'have been moved to', self.destination
+                self.files = ttk.Label(self.window, text = confirmation, justify = CENTER, font = ('Courier', 13, 'bold'),wraplength = 600).pack()
+                print('Text files from', source, 'have been moved to', self.destination)
             else:
                 print('finished')
         else:
             print('none')
 
+def file_activator(self):
+    if self.path != '':
+        self.button.config(state='normal')
 
+def execute_activator(self):
+    if self.path != '' and self.destination != '':
+        self.executer.config(state='normal')
+    else:
+        self.executer.config(state='disabled')
+
+def con_prompt(self):
+    self.window = Toplevel(self.panedwindow)
+    self.window.resizable(True,True)
+    self.window.geometry('600x600+100+100')
+
+def show_source(self):
+    self.field1.config(text = "Source Folder: " + self.path)
+
+def show_destination(self):
+    self.field2.config(text = "Destination Folder: " + self.destination)
+    
 
 if __name__ == "__main__":
     pass
