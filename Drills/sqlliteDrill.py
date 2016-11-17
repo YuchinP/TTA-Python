4dc1e33d00a6e53111c9f1c05773a018ffc5e4b2
Yuchin Pan <yuchinspan@gmail.com>
2016-11-16 16:51:16 -0800

    Final TTA Python Drill
    Added sqlite database for timestamps

---
+++ Drills/sqlliteDrill.py
@@ -0,0 +1,61 @@
+from tkinter import *
+from tkinter import ttk
+
+import sqlliteFunc
+
+
+class ShutilUI(Frame):
+    
+    def __init__(self,root):
+
+        Frame.__init__(self,root)
+
+        #   Title of the UI
+        self.label = ttk.Label(root, text = "File Moving UI", justify = CENTER, font = ('Courier', 13, 'bold'))
+        self.label.pack()
+
+        #   Creation of the paned window
+        self.panedwindow = ttk.Panedwindow(root, orient = VERTICAL)
+        self.panedwindow.pack(expand = False)
+        self.frame1 = ttk.Frame(self.panedwindow, width = 600, height = 400)
+        self.frame2 = ttk.Frame(self.panedwindow, width = 600, height = 200)
+        self.frame3 = ttk.Frame(self.panedwindow, width = 600, height = 50, relief = SUNKEN)
+        self.frame4 = ttk.Frame(self.panedwindow, width = 600, height = 50, relief = SUNKEN)
+        self.frame5 = ttk.Frame(self.panedwindow, width = 600, height = 25)
+        self.panedwindow.add(self.frame1, weight = 1)
+        self.panedwindow.add(self.frame2, weight = 4)
+        self.panedwindow.add(self.frame3, weight = 4)
+        self.panedwindow.add(self.frame4, weight = 4)
+        self.panedwindow.add(self.frame5, weight = 1)
+
+        #   Frame1 instructions
+        self.instructions = ttk.Label(self.frame1, text = "In order to execute the file transfer for files modified within the last 24 hours, please select an initial and destination folder. You may check eligible files for the file transfer with the 'Show Eligible Files' button after selecting an initial folder", justify = CENTER, font = ('Courier', 12))
+        self.instructions.config(wraplength = 600)
+        self.instructions.pack()
+
+        #   Source/Destinations
+        self.field1 = ttk.Label(self.frame3, text = "Source Folder:")
+        self.field2 = ttk.Label(self.frame4, text = "Destination Folder:")
+        self.field1.grid()
+        self.field2.grid()
+        
+        #   Define buttons
+        self.browse = ttk.Button(self.frame2, text = "Browse Initial Folder", command = lambda:sqlliteFunc.folderselect1(self))
+        self.browse2 = ttk.Button(self.frame2, text = "Browse Destination Folder", command = lambda:sqlliteFunc.folderselect2(self))
+        self.button = ttk.Button(self.frame2, text = "Show Eligible Files", command = lambda:sqlliteFunc.show_files(self), state = 'disabled')
+        self.executer = ttk.Button(self.frame2, text = "Execute File Transfer", command = lambda:sqlliteFunc.the_mover(self), state = 'disabled')
+
+        #   Placing buttons down on grid
+        self.browse.grid(row = 0, column = 1, padx = 40)
+        self.browse2.grid(row = 0, column = 2, padx = 40)
+        self.button.grid(row = 0, column = 3, padx = 40)
+        self.executer.grid(row = 0, column = 4, padx = 40)
+        
+        self.path = ''      
+        self.destination = ''
+        
+if __name__=='__main__':
+    root = Tk()
+    ShutilUI(root).pack()
+    root.mainloop()
+
---
+++ Drills/sqlliteFunc.py
@@ -0,0 +1,116 @@
+import os
+import shutil
+from datetime import *
+from tkinter import *
+from tkinter import ttk
+import sqlite3
+
+#   The UI and Main Python
+import sqlliteDrill
+
+#   Create the Database if it doesn't already exist for the timestamps
+def create_db(self):
+    conn = sqlite3.connect('timestamp.db')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE if not exists tbl_timestamp( \
+    ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+    tsp TIMESTAMP \
+    );")
+        conn.commit()
+    conn.close()
+
+#   Defines the browsing and selecting for the folders
+def folderselect1(self):
+    Sel_directory1 = filedialog.askdirectory()
+    print(Sel_directory1)
+    self.path = Sel_directory1
+    file_activator(self) #Activates the Eligible Files button
+    execute_activator(self) #Checks one of two requirements to activate the execute files button
+    show_source(self) # Displays the Source path    
+        
+def folderselect2(self): 
+    Sel_directory2 = filedialog.askdirectory()
+    print(Sel_directory2)
+    self.destination = Sel_directory2
+    execute_activator(self)
+    show_destination(self)
+
+#   Gets the converted time of last modification on files             
+def get_time(self,files):
+    files = os.path.join(self.path, files)
+    mtime = os.path.getmtime(files)
+    ntime = datetime.fromtimestamp(mtime)
+    now = datetime.now()
+    tdelta = now - ntime
+    ntimer = tdelta.total_seconds()
+    return ntimer
+
+#   This defines and will show the files modified within the last 24 hours
+def show_files(self):
+    con_prompt(self) #Opens a prompt that tells the files eligible
+    for files in os.listdir(self.path):
+        if files.endswith('.txt'):
+            get_time(self,files)
+            if get_time(self,files) < 86400:
+                print (files)
+                output = files
+                self.files = ttk.Label(self.window, text = output, justify = CENTER, font = ('Courier', 13, 'bold')).pack()
+            else:
+                print('Ineligible')
+
+#   The main file mover         
+def the_mover(self):
+    create_db(self)
+    source = self.path
+    con_prompt(self)
+    for files in os.listdir(self.path):
+        files = os.path.join(source, files)
+        if files.endswith('.txt'):
+            get_time(self,files)
+            if get_time(self,files) < 86400:
+                shutil.move(files, self.destination)
+                confirmation = 'Text files from', source, 'have been moved to', self.destination
+                self.files = ttk.Label(self.window, text = confirmation, justify = CENTER, font = ('Courier', 13, 'bold'),wraplength = 600).pack()
+                print('Text files from', source, 'have been moved to', self.destination)
+                timestamp = str(datetime.now()) 
+                conn = sqlite3.connect('timestamp.db')
+                with conn:
+                    cursor = conn.cursor()
+                    cursor.execute("INSERT INTO tbl_timestamp (tsp) VALUES (?)", (datetime.now(),))
+                    recent = cursor.execute("SELECT tsp FROM tbl_timestamp ORDER BY tsp DESC LIMIT 1").fetchone()
+                    print (str(recent))
+                    self.timestamp = ttk.Label(self.frame5, text = "Last File Check at : " + str(recent)).pack()
+                    conn.commit()
+                conn.close()                             
+            else:
+                print('finished')
+
+#   Activates the Eligible files button if a initial folder is selected
+def file_activator(self):
+    if self.path != '':
+        self.button.config(state='normal')
+
+#   Activates the execute button if both folders are selected
+def execute_activator(self):
+    if self.path != '' and self.destination != '':
+        self.executer.config(state='normal')
+    else:
+        self.executer.config(state='disabled')
+
+#   Creates a popup
+def con_prompt(self):
+    self.window = Toplevel(self.panedwindow)
+    self.window.resizable(True,True)
+    self.window.geometry('600x600+100+100')
+
+#   Shows the paths to the selected folders
+def show_source(self):
+    self.field1.config(text = "Source Folder: " + self.path)
+
+def show_destination(self):
+    self.field2.config(text = "Destination Folder: " + self.destination)
+    
+
+if __name__ == "__main__":
+    pass
